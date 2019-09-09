cd37418570cfd4a5be217f18d3b50f2207736b31
vagrant <vagrant@machine1.puphpet>
2019-09-09 07:51:37 +0000

    added multiline file support

--- makewords.go
+++ makewords.go
@@ -6,14 +6,16 @@ import (
 	"strings"
 )
 func main() {
-    reader := bufio.NewReader(os.Stdin);
-    text, _ := reader.ReadString('\n');
-    words := strings.Split(text, " ");
-    
-    for i := 0; i < len(words); i++ {
-        word := words[i];
-        wordWithoutDot := strings.Replace(word, ".", "", -1);
-        wordWithoutComma := strings.Replace(wordWithoutDot, ",", "", -1);
-    	fmt.Println(wordWithoutComma);
+    scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan() {
+        text := scanner.Text();
+        words := strings.Split(text, " ");
+        
+        for i := 0; i < len(words); i++ {
+            word := words[i];
+            wordWithoutDot := strings.Replace(word, ".", "", -1);
+            wordWithoutComma := strings.Replace(wordWithoutDot, ",", "", -1);
+            fmt.Println(wordWithoutComma);
+        }
     }
 }
